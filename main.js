8e034b9ade248d553df7202ae83b183438f9ca98
gerijeb <gtpepi@hotmail.com>
2022-01-13 18:46:32 -0300

    Add rainbow mode

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const body = document.querySelector("body");
 createBaseHtml();
 createContainer();
 
-const defaultColor = "black";
-let gridColor = defaultColor; 
+const defaultMode = 'color'
+let colorGridMode = defaultMode;
 
 
 function createBaseHtml () {
@@ -31,11 +31,18 @@ function createContainer () {
     
     const switchWhiteBtn = document.createElement("button");
     switchWhiteBtn.setAttribute("class", "switchWhiteBtn");
+    switchWhiteBtn.textContent = "Erase mode!";
     lateralDiv.prepend(switchWhiteBtn);
 
     const switchBlackBtn = document.createElement("button");
     switchBlackBtn.setAttribute("class", "switchBlackBtn");
+    switchBlackBtn.textContent = "Switch to Black!";
     lateralDiv.appendChild(switchBlackBtn);
+
+    const switchRandBtn = document.createElement("button");
+    switchRandBtn.setAttribute("class", "randBtn");
+    switchRandBtn.textContent = "RANDOM COLORS!";
+    lateralDiv.appendChild(switchRandBtn);
 
     const containerDiv = document.createElement("div");
     containerDiv.setAttribute("class", "container");
@@ -55,27 +62,45 @@ function createGrid (containerDiv) {
         cloneDiv.addEventListener("mouseover", paintGrid);
         containerDiv.appendChild(cloneDiv);  
     }
-
-    styleGrid();
+    addCSS();
 };
 
-function styleGrid () {
+function addCSS () {
     const lateralDiv = document.querySelector(".lateral")
     lateralDiv.style.display = 'flex';
     lateralDiv.style.flexDirection = "column";
+    lateralDiv.style.gap = "2rem";
 
     const gridContainer = document.querySelector('.container');
     gridContainer.style.gridTemplateColumns = `repeat(16, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(16, 1fr)`;
 
-}
+    const switchWhiteBtn = document.querySelector(".switchWhiteBtn");
+    switchWhiteBtn.addEventListener("click", () => colorGridMode = 'erase');
+
+    const switchBlackBtn = document.querySelector(".switchBlackBtn");
+    switchBlackBtn.addEventListener("click", () => colorGridMode = 'color');
+ 
+    const switchRandBtn = document.querySelector(".randBtn");
+    switchRandBtn.addEventListener("click", () => colorGridMode = 'random');
+};
 
 function paintGrid (e) {
-    e.target.style.backgroundColor = `${gridColor}`
-}
+    console.log(colorGridMode);
+    if (colorGridMode == 'random') {
+        let randR = Math.floor(Math.random() * 256);
+        let randG = Math.floor(Math.random() * 256);
+        let randB = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randR}, ${randG}, ${randB})`
+        
+    } else if (colorGridMode == 'color') {
+        e.target.style.backgroundColor = 'black';
+    } else if (colorGridMode == 'erase') {
+        e.target.style.backgroundColor = 'white';
+    }
 
-function switchColors (color) {
-    gridColor = `${color}`    
+
+
 };
 
 
