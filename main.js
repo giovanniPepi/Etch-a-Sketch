d1f168f3b0b14ab72b286633ca9ce5d77e075e67
gerijeb <gtpepi@hotmail.com>
2022-01-13 22:44:40 -0300

    Add final CSS version

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ createHtml();
 addCSS();
 getEventListeners();
 
-
 function createHtml () {
     const headerDiv  = document.createElement("div");
     headerDiv.setAttribute("class", "headerDiv");
@@ -27,35 +26,35 @@ function createHtml () {
     
     const switchWhiteBtn = document.createElement("button");
     switchWhiteBtn.setAttribute("class", "switchWhiteBtn");
-    switchWhiteBtn.textContent = "Erase mode!";
-    lateralDiv.prepend(switchWhiteBtn);
+    switchWhiteBtn.textContent = "Eraser";
+    lateralDiv.appendChild(switchWhiteBtn);
 
     const switchBlackBtn = document.createElement("button");
     switchBlackBtn.setAttribute("class", "switchBlackBtn");
-    switchBlackBtn.textContent = "Switch to Black!";
+    switchBlackBtn.textContent = "Dark Pen";
     lateralDiv.appendChild(switchBlackBtn);
-
-    const switchRandBtn = document.createElement("button");
-    switchRandBtn.setAttribute("class", "randBtn");
-    switchRandBtn.textContent = "RANDOM COLORS!";
-    lateralDiv.appendChild(switchRandBtn);
 
     const switchPencilBtn = document.createElement("button");
     switchPencilBtn.setAttribute("class", "switchPencilBtn");
-    switchPencilBtn.textContent = "PENCIL MODEEEEEEEEE";
+    switchPencilBtn.textContent = "Pencil";
     lateralDiv.appendChild(switchPencilBtn)
+
+    const switchRandBtn = document.createElement("button");
+    switchRandBtn.setAttribute("class", "randBtn");
+    switchRandBtn.textContent = "Random Color";
+    lateralDiv.appendChild(switchRandBtn);
+
+    const eraseBtn = document.createElement("button");
+    eraseBtn.setAttribute("class", "eraseBtn");
+    eraseBtn.textContent = "Erase";
+    lateralDiv.appendChild(eraseBtn);
 
     const containerDiv = document.createElement("div");
     containerDiv.setAttribute("class", "containerDiv");
 
-    mainDiv.prepend(lateralDiv);
-    mainDiv.appendChild(containerDiv);     
-
-    createGrid(containerDiv);     
-
-    
-
-   
+    mainDiv.appendChild(lateralDiv);
+    mainDiv.prepend(containerDiv);     
+    createGrid(containerDiv);       
 };
 
 function createGrid (containerDiv) {
@@ -75,59 +74,61 @@ function refreshClones () {
 
 function addCSS () {
     const body = document.querySelector("body");
-    body.style.background = "rgb(171, 186, 197)";
+    body.style.background = "rgb(224, 227, 230)";
     body.style.height = '100vh';
     body.style.display = 'flex';
     body.style.flexDirection = 'column';
     body.style.margin = 0;
     body.style.textAlign = 'center';
     body.style.fontFamily = 'Arial';
+    body.style.color = 'rgb(65, 65, 65';
 
     const lateralDiv = document.querySelector(".lateral")
     lateralDiv.style.display = 'flex';
-    lateralDiv.style.flexDirection = "column";
-    lateralDiv.style.gap = "2rem";
+    lateralDiv.style.gap = "1rem";
 
     const mainDiv = document.querySelector(".mainDiv");
     mainDiv.style.display = "flex";
-    mainDiv.style.background = "rgb(171, 186, 197)";
+    mainDiv.style.flexDirection = "column";
+    mainDiv.style.background = "rgb(224, 227, 230)"; 
     mainDiv.style.flex = 1;
-    mainDiv.style.gap = '5rem';
-    mainDiv.style.justifyContent = 'center';
+    mainDiv.style.gap = '1rem';
+    mainDiv.style.justifyContent = 'flex-start';
     mainDiv.style.alignItems = 'center';
     mainDiv.style.margin = "0 auto";
-    mainDiv.style.maxWidth = '60rem';
-    mainDiv.style.padding = '4rem';
+    mainDiv.style.maxWidth = '55rem';
+    mainDiv.style.padding = '1rem';
     
     const containerDiv = document.querySelector('.containerDiv');
     containerDiv.style.display = 'grid';
     containerDiv.style.gridTemplateColumns = `repeat(16, 1fr)`;
     containerDiv.style.gridTemplateRows = `repeat(16, 1fr)`; 
-    containerDiv.style.height = '40rem';
-    containerDiv.style.width = '40rem';
+    containerDiv.style.height = '37rem';
+    containerDiv.style.width = '37rem';
     
     const headerDiv = document.querySelector(".headerDiv")
     const headerDivP = document.createElement("p");
     headerDivP.textContent = "Etch-a-Sketch";
     headerDiv.appendChild(headerDivP);
 
-    headerDiv.style.backgroundColor = "rgb(94, 119, 138)";
+    headerDiv.style.backgroundColor = "background:rgb(173, 181, 197)";
     headerDiv.style.height = "5rem";
     headerDiv.style.display = "flex";
-    headerDiv.style.padding = "1rem";
+    headerDiv.style.padding = "1.5rem";
     headerDiv.style.alignText = "center";
     headerDiv.style.alignItems = "center";
     headerDiv.style.justifyContent = "center";
-    headerDiv.style.fontSize = "3rem";
+    headerDiv.style.fontSize = "6rem";
     headerDiv.style.fontWeight = 550;
  
     const footerDiv = document.querySelector(".footerDiv")
     const footerDivP = document.createElement("p");
-    footerDivP.textContent = "I'm the footer now";
+    footerDiv.textContent = "Copyright @gerijeb"
     footerDiv.appendChild(footerDivP);
 
     footerDiv.style.backgroundColor = "rgb(94, 119, 138)";
     footerDiv.style.height = "1rem";
+    footerDiv.style.fontSize = '1.25rem';
     footerDiv.style.display = "flex";
     footerDiv.style.padding = "1rem";
     footerDiv.style.alignText = "center";
@@ -135,8 +136,18 @@ function addCSS () {
     footerDiv.style.justifyContent = "center";
 
     const cloneDiv = document.querySelectorAll(".cloneDiv");
-    cloneDiv.forEach(clone => clone.style.border = ".1rem solid black");
     cloneDiv.forEach (clone => clone.style.backgroundColor = 'white'); 
+
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach(button => button.style.background = 'background:rgb(173, 181, 197)');
+    buttons.forEach(button => button.style.fontSize ='1rem');
+    buttons.forEach(button => button.style.border = '.1rem solid rgb(65, 65, 65)');
+    buttons.forEach(button => button.style.borderRadius = '.5rem');
+    buttons.forEach(button => button.style.color = "rgb(65, 65, 65)");
+    buttons.forEach(button => button.style.padding = '1rem');
+
+
+
 };
 
 function getEventListeners() {
@@ -160,6 +171,9 @@ function getEventListeners() {
         colorGridMode = "pencil";
         refreshClones();
     });
+
+    const eraseBtn = document.querySelector(".eraseBtn");
+    eraseBtn.addEventListener("click", () => refreshClones());
 };
 
 function paintGrid (e) {
@@ -169,7 +183,7 @@ function paintGrid (e) {
         let randB = Math.floor(Math.random() * 256);
         e.target.style.backgroundColor = `rgb(${randR}, ${randG}, ${randB})`        
     } else if (colorGridMode == 'color') {
-        e.target.style.backgroundColor = 'black';
+        e.target.style.backgroundColor = 'rgb(65, 65, 65)';
     } else if (colorGridMode == 'erase') {
         e.target.style.backgroundColor = 'white';
     } else if (colorGridMode = 'pencil') {
